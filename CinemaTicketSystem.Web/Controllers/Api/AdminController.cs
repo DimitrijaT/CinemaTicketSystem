295db2fa0f2c503929c12ccc8a9e572c52a382a1
Димитрија Тимески <32930367+DimitrijaT@users.noreply.github.com>
2023-07-10 11:54:29 +0200

    Add Admin Controller for sending Orders and importing Users

--- CinemaTicketSystem.Web/Controllers/Api/AdminController.cs
+++ CinemaTicketSystem.Web/Controllers/Api/AdminController.cs
@@ -1,7 +1,14 @@
-﻿using CinemaTicketSystem.Domain.DomainModels;
+﻿
+
+using CinemaTicketSystem.Domain.DomainModels;
+using CinemaTicketSystem.Domain.Dto;
+using CinemaTicketSystem.Domain.Identity;
 using CinemaTicketSystem.Service.Interface;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
 
 namespace CinemaTicketSystem.Web.Controllers.Api
 {
@@ -10,10 +17,12 @@ namespace CinemaTicketSystem.Web.Controllers.Api
     public class AdminController : ControllerBase
     {
         private readonly IOrderService _orderService;
+        private readonly UserManager<CinemaTicketSystemUser> _userManager;
 
-        public AdminController(IOrderService orderService)
+        public AdminController(IOrderService orderService, UserManager<CinemaTicketSystemUser> userManager)
         {
             _orderService = orderService;
+            this._userManager = userManager;
         }
 
         [HttpGet("[action]")]
@@ -27,6 +36,46 @@ namespace CinemaTicketSystem.Web.Controllers.Api
         public Order GetDetailsForOrder(BaseEntity model)
         {
             return this._orderService.getOrderDetails(model);
+        }
+
+
+        // Get a list of users and register them into the database
+
+        // Note: UserRegistrationDto is a built in class in .NET
+
+        [HttpPost("[action]")]
+        public bool ImportAllUsers(List<UserRegistrationDto> model)
+        {
+            bool status = true;
+            foreach (var item in model)
+            {
+                // First we check if the account exists.
+                var userCheck = _userManager.FindByEmailAsync(item.Email).Result;
+                if (userCheck == null)
+                {
+                    var user = new CinemaTicketSystemUser
+                    {
+                        FirstName = item.FirstName,
+                        LastName = item.LastName,
+                        UserName = item.Email,
+                        NormalizedUserName = item.Email,
+                        Email = item.Email,
+                        EmailConfirmed = true,
+                        PhoneNumberConfirmed = true,
+                        PhoneNumber = item.PhoneNumber,
+                        UserCart = new ShoppingCart()
+                    };
+                    var result = _userManager.CreateAsync(user, item.Password).Result;
+
+                    // All users must succeed in creating.
+                    status = status && result.Succeeded;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            return status;
         }
     }
 
